ec9f3a7212fa2ef65e6655bf3ac8d818949675f9
Manuela Gomez <mmanu.gomez@gmail.com>
2019-10-30 10:22:37 -0400

    [wip] github helper importer and options set correctly with input username

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,22 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
-
-  // The options object has been provided to help you out, 
+let getReposByUsername = (err, req, res, next) => {
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
+  var username = req.body.username;
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
+  console.log('URL', options.url);
+  // next();
+  // request.get(options, (error, response) => {
+  //   if (error)
+  // });
 }
 
 module.exports.getReposByUsername = getReposByUsername;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const gitHub = require('../helpers/github.js');
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
@@ -11,7 +12,9 @@ app.use(bodyParser.json());
 
 app.post('/repos', function (req, res) {
   // req.body -> {username 'search term'}
-
+  //gitHub.getReposByUserName(req.body.username);
+  gitHub.getReposByUsername(null, req);
+  res.end();
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
